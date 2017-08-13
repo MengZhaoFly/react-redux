83042c9691da0105a91fadea0cd17ef8582bde03
赵猛 <zhaomeng@zhaomengdeMacBook-Pro.local>
2017-08-13 18:16:25 +0800

    add map state and pass to props, add high Component

--- src/connect.js
+++ src/connect.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
 import propTypes from 'prop-types'
-export const connect = (WrapperedComponent) => {
+
+export const connect = (mapStateToProps) => (WrapperedComponent) => {
     class Connect extends Component {
         static contextTypes = {
             store: propTypes.object
         }
-        
+        constructor() {
+            super()
+            this.state = {}
+        }
+        componentWillMount() {
+            const { store } = this.context
+            this.setState = ({
+                store
+            })
+        }
+        render() {
+            return (
+                <WrapperedComponent data={ this.state.store }/>
+            )
+        }
     }
+    return Connect
 }
