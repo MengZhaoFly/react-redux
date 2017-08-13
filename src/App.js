107f409862f77cff7a4b798ef731154db86564f9
赵猛 <zhaomeng@zhaomengdeMacBook-Pro.local>
2017-08-13 20:01:17 +0800

    context交给provide处理

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
 import React, { Component } from 'react';
 import Header from './Header'
 import Content from './Content'
-import CreateStore from './CreateStore'
-import propTypes from 'prop-types'
-const ThemeReducer = (state, action) => {
-  if (!state) {
-    return {
-      themeColor: 'red'
-    }
-  }
-  switch (action.type) {
-    case 'CHANGE_COLOR':
-      return {
-        ...state,
-        themeColor: action.color
-      }
-    default: 
-      return state
-  }
-}
-const store = CreateStore(ThemeReducer)
+// import CreateStore from './CreateStore'
+// import propTypes from 'prop-types'
+
 class App extends Component {
-  static childContextTypes = {
-    store: propTypes.object
-  }
-  getChildContext () {
-    return { store }
-  }
+  // 交给provide处理
+  // static childContextTypes = {
+  //   store: propTypes.object
+  // }
+  // getChildContext () {
+  //   return { store }
+  // }
   render() {
     return (
       <div className="App">
--- src/index.js
+++ src/index.js
@@ -3,6 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Provider from './Provider'
+import CreateStore from './CreateStore'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const ThemeReducer = (state, action) => {
+  if (!state) {
+    return {
+      themeColor: 'red'
+    }
+  }
+  switch (action.type) {
+    case 'CHANGE_COLOR':
+      return {
+        ...state,
+        themeColor: action.color
+      }
+    default: 
+      return state
+  }
+}
+const store = CreateStore(ThemeReducer)
+
+ReactDOM.render(<Provider store={store}> <App /> </Provider>, document.getElementById('root'));
 registerServiceWorker();
